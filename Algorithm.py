bcf3c31682d4f161208a519e3ca54daf6a1416f9
Sobolevsky.A <Andrey.Sobolevsky@ulb.ac.be>
2021-03-14 15:15:42 +0100

    example where one edge is changing color every loop

--- Algorithm.py
+++ Algorithm.py
@@ -11,11 +11,18 @@ class Algorithm:
         # edges is a list of tuples (source, destination, weight, color)
         # nodes is a dictionary that stores the index of each edges in the list above
         # nodes is a dictionary where we can get direct neighbors of each nodes and the color assigned to each node
+
+        # if you want to solve before visualizing the process you can do it here, or you can solve during the
+        # visualization in the update function
+
+        # you can delete this it is for the example presented in the update function below
+        self.color = ["blue", "red", "green", "m"]
+        self.counter = 0
+
         self.view = View(self, label_edges=True, speed=20)
 
     def getEdges(self):
         return self.edges
-
 
     def setEdgeColor(self, src, dest, color):
         if (src, dest) in self.edge_index:
@@ -33,6 +40,9 @@ class Algorithm:
         updated constantly during each refresh of the view
         :return: void
         """
+        # example where one edge is changing color every tick
+        self.setEdgeColor(1, 2, self.color[self.counter%4])
+        self.counter += 1
 
 
 algorithm = Algorithm("datasets/example")
