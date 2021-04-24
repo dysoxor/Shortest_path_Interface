6df0f568c0cabacf4ae690ef1d8c5d19665f4985
Sobolevsky.A <Andrey.Sobolevsky@ulb.ac.be>
2021-04-24 19:22:34 +0200

    remove useless

--- main.py
+++
@@ -1,9 +0,0 @@
-import dash
-import visdcc
-import pandas as pd
-import dash_core_components as dcc
-import dash_core_components as html
-from dash.dependencies import Input, Output, State
-
-app = dash.Dash()
-
