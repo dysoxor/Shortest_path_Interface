8251f113ac991d917b8ce1ae8c8d4a3ac1f749b9
Sobolevsky.A <Andrey.Sobolevsky@ulb.ac.be>
2021-04-24 19:20:19 +0200

    created instance with 20 nodes, set fixed position of nodes (to exploit position in heuristic)

---
+++ main.py
@@ -0,0 +1,9 @@
+import dash
+import visdcc
+import pandas as pd
+import dash_core_components as dcc
+import dash_core_components as html
+from dash.dependencies import Input, Output, State
+
+app = dash.Dash()
+
